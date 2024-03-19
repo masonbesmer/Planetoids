54ee7229846c7f45e12a7b7993b1644862508c55
Mason Besmer <masonbesmer@gmail.com>
2024-03-19 11:14:32 -0500

    more mixin work stil borken

--- src/main/java/org/masonbesmer/planetoids/forge/mixin/MixinGameModeSwitcherScreen.java
+++ src/main/java/org/masonbesmer/planetoids/forge/mixin/MixinGameModeSwitcherScreen.java
@@ -5,8 +5,10 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.debug.GameModeSwitcherScreen;
+import net.minecraft.client.telemetry.TelemetryProperty;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -31,7 +33,9 @@ public abstract class MixinGameModeSwitcherScreen extends Screen {
     @Shadow
     protected abstract boolean checkToClose();
     @Shadow
-    private Optional<?> currentlyHovered;
+    //private Object currentlyHovered;
+    //@Shadow
+    private Optional<GameModeIcon> currentlyHovered;
 
     @Shadow
     private boolean setFirstMousePos;
@@ -42,30 +46,37 @@ public abstract class MixinGameModeSwitcherScreen extends Screen {
     @Shadow
     private int firstMouseY;
 
-    @Inject(at = @At(value = "HEAD"), method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;IIF)V")
+    @Shadow
+    private static GameModeSwitcherScreen $$enum$GameModeIcon;
+
+    @Inject(at = @At(value = "HEAD"), method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;IIF)V", cancellable = true)
     private void onRender(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick, CallbackInfo info) {
         info.cancel();
+
+        GameModeSwitcherScreen s = (GameModeSwitcherScreen)((Object)this);
+
+        System.out.println("hello from mixin!!!");
 
         if (!this.checkToClose()) {
             pPoseStack.pushPose();
             RenderSystem.enableBlend();
             RenderSystem.setShaderTexture(0, GAMEMODE_SWITCHER_LOCATION);
-            int i = this.width / 2 - 62;
-            int j = this.height / 2 - 31 - 27;
+            int i = s.width / 2 - 62;
+            int j = s.height / 2 - 31 - 27;
             blit(pPoseStack, i, j, 0.0F, 0.0F, 125, 75, 128, 128);
             pPoseStack.popPose();
             super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
-            this.currentlyHovered.ifPresent((p_274676_) -> {
-                drawCenteredString(pPoseStack, this.font, p_274676_.getName(), this.width / 2, this.height / 2 - 31 - 20, -1);
+            this.currentlyHovered.ifPresent((fws) -> {
+                drawCenteredString(pPoseStack, s.font, fws::getName(), s.width / 2, s.height / 2 - 31 - 20, -1);
             });
-            drawCenteredString(pPoseStack, this.font, SELECT_KEY, this.width / 2, this.height / 2 + 5, 16777215);
+            drawCenteredString(pPoseStack, s.font, SELECT_KEY, s.width / 2, s.height / 2 + 5, 16777215);
             if (!this.setFirstMousePos) {
-                this.firstMouseX = pMouseX;
-                this.firstMouseY = pMouseY;
-                this.setFirstMousePos = true;
+                firstMouseX = pMouseX;
+                firstMouseY = pMouseY;
+                setFirstMousePos = true;
             }
 
-            boolean flag = this.firstMouseX == pMouseX && this.firstMouseY == pMouseY;
+            boolean flag = firstMouseX == pMouseX && firstMouseY == pMouseY;
 
             for(GameModeSwitcherScreen.GameModeSlot gamemodeswitcherscreen$gamemodeslot : this.slots) {
                 gamemodeswitcherscreen$gamemodeslot.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
